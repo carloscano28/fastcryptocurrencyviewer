2182426a7e821897f524f84023e49fdd16695f54
c.r.cano <c.r.cano@outlook.com>
2023-02-23 20:52:57 -0600

    - Mejoras al proyecto

--- app/src/main/java/com/example/fastcryptocurrencyviewer/ui/view/AvailableBooksFragment.kt
+++ app/src/main/java/com/example/fastcryptocurrencyviewer/ui/view/AvailableBooksFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fastcryptocurrencyviewer.R
@@ -17,7 +16,6 @@ import com.example.fastcryptocurrencyviewer.ui.adapters.AvailableBooksAdapter
 import com.example.fastcryptocurrencyviewer.utils.Utils
 import com.example.fastcryptocurrencyviewer.ui.viewmodel.AvailableBooksVM
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class AvailableBooksFragment : Fragment() {
@@ -36,10 +34,7 @@ class AvailableBooksFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        availableBooksVM.getAvailableBookInvoke()
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            availableBooksVM.stateAvailableLD.observe(viewLifecycleOwner) {
+            availableBooksVM.getAvailableBookLiveData().observe(viewLifecycleOwner) { it ->
                 val adaptador = AvailableBooksAdapter(it.characters) {
                     findNavController().navigate(
                         R.id.action_availableBooksFragment_to_detailFragment,
@@ -54,6 +49,21 @@ class AvailableBooksFragment : Fragment() {
                     Utils.errorDialog(requireContext(), it.errorMessage)
                 }
             }
-        }
+
+            /*availableBooksVM.stateAvailableLD.observe(viewLifecycleOwner) {
+                val adaptador = AvailableBooksAdapter(it.characters) {
+                    findNavController().navigate(
+                        R.id.action_availableBooksFragment_to_detailFragment,
+                        bundleOf("book" to it)
+                    )
+                }
+                binding.rvAvailableBooks.layoutManager = LinearLayoutManager(activity)
+                binding.rvAvailableBooks.adapter = adaptador
+                binding.progressBar.isVisible = it.isLoading
+
+                if (!it.errorMessage.isNullOrEmpty()) {
+                    Utils.errorDialog(requireContext(), it.errorMessage)
+                }
+            }*/
     }
 }
--- app/src/main/java/com/example/fastcryptocurrencyviewer/ui/viewmodel/AvailableBooksVM.kt
+++ app/src/main/java/com/example/fastcryptocurrencyviewer/ui/viewmodel/AvailableBooksVM.kt
@@ -1,16 +1,10 @@
 package com.example.fastcryptocurrencyviewer.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.fastcryptocurrencyviewer.data.domain.AvailableBooksUseCase
 import com.example.fastcryptocurrencyviewer.utils.*
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,10 +14,20 @@ class AvailableBooksVM @Inject constructor(private val availableBooksUseCase: Av
     private val _stateAvailableLD = MutableLiveData<Utils.AvailableBooksUiState>()
     val stateAvailableLD: LiveData<Utils.AvailableBooksUiState> = _stateAvailableLD
 
-    fun getAvailableBookInvoke(){
+    /*fun getAvailableBookInvoke(){
         viewModelScope.launch {
             val result = availableBooksUseCase.invoke()
             _stateAvailableLD.value = Utils.AvailableBooksUiState(isLoading = false, characters = result )
         }
+    }*/
+
+    fun getAvailableBookLiveData() = liveData(viewModelScope.coroutineContext + Dispatchers.Main){
+        emit(
+            Utils
+            .AvailableBooksUiState(
+                isLoading = false,
+                characters = availableBooksUseCase.invoke()
+            )
+        )
     }
 }
