e58e3bfb043e202da16d15c70b22bfdb5d3732a7
c.r.cano <c.r.cano@outlook.com>
2023-03-02 12:20:49 -0600

    - Se agrega primera version de flecha de retorno para la navegacion

--- app/src/main/java/com/example/fastcryptocurrencyviewer/MainActivity.kt
+++ app/src/main/java/com/example/fastcryptocurrencyviewer/MainActivity.kt
@@ -2,15 +2,28 @@ package com.example.fastcryptocurrencyviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+
 import com.example.fastcryptocurrencyviewer.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Codigo para agregar flecha de retorno
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
